d5504a1ac7866f467d28bd5e1eb30a305e27a20c
Peter Koss <peter.koss@fys.kuleuven.be>
2016-01-14 16:56:38 +0100

    Code can produce points for one pump pulse for given field strength and duty cycle.

--- pump_probe.py
+++ pump_probe.py
@@ -1,12 +1,29 @@
+# Values valid for all pump-probe waveforms
 gammaL = 3500 # Gyromagnetic ratio of Cs 
+totalTime = 100 # Duration of the total pump-probe cycle
+totalPoints = 100000 # Total number of points in the waveform
+
 
 fieldStrength = 1 # B-field strength in uT
 pumpTime = 30 # Duration of the pumping
-totalTime = 100 # Duration of the total pump-probe cycle
 pumpAmp = 0.5 # Amplitude of the pump in fractions of set Vpp
 probeAmp = 0.03 # Amplitude of the probe in fractions of set Vpp
+dutyCycle = 0.1
 
 nuL = fieldStrength * gammaL
 periodL = 1/nuL
+periodPoints = periodL * totalPoints
+dutyPoints = periodPoints * dutyCycle
 
-print(periodL)
+singlePumpCycle = []
+pumpProbeCycle = []
+
+cyclePoints = 0
+while (cyclePoints < periodPoints):
+    if (cyclePoints < dutyPoints):
+        singlePumpCycle.append(pumpAmp)
+    else:
+        singlePumpCycle.append(0.0)
+    cyclePoints += 1
+
+print(singlePumpCycle)
