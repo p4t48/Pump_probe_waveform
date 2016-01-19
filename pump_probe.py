40ed43e193570f58bac3134b7c06b6a7c49d4b6a
Peter Koss <peter.koss@fys.kuleuven.be>
2016-01-19 16:32:27 +0100

    The script is now able to generate a pump probe waveform on the signal generator.

--- pump_probe.py
+++ pump_probe.py
@@ -4,24 +4,23 @@ import usbtmc
 
 # Values valid for all pump-probe waveforms
 gammaL = 3500 # Gyromagnetic ratio of Cs (3500 Hz/uT)
-totalTime = 100 # Duration of the total pump-probe cycle
+totalTime = 0.1 # Duration of the total pump-probe cycle in s
 totalPoints = 100000 # Total number of points in the waveform
 
 
 fieldStrength = 1 # B-field strength in uT
-pumpTime = 25 # Duration of the pumping
+pumpTime = 0.025 # Duration of the pumping in s
 pumpAmp = 1 # Amplitude of the pump in fractions of set Vpp
 probeAmp = 0.03 # Amplitude of the probe in fractions of set Vpp
 dutyCycle = 0.1
 
 nuL = fieldStrength * gammaL
 periodL = 1/nuL
-periodPoints = periodL * totalPoints
-dutyPoints = periodPoints * dutyCycle
-pumpPoints = m.floor((pumpTime/totalTime) * totalPoints)
 
+# Points required to generate a single pump pulse
+periodPoints = m.floor(periodL/totalTime * totalPoints)
+dutyPoints = periodPoints * dutyCycle
 singlePumpCycle = []
-pumpProbeCycle = []
 
 cyclePoints = 0
 while (cyclePoints < periodPoints):
@@ -31,15 +30,6 @@ while (cyclePoints < periodPoints):
         singlePumpCycle.append(0.0)
     cyclePoints += 1
 
-#while (len(pumpProbeCycle) < totalPoints):
-#    pumpProbeCycle.extend([probeAmp])
-
-
-# Plot the generated pump-probe cycle
-xVals = list(range(len(singlePumpCycle)))
-plt.plot(xVals,singlePumpCycle,'o')
-#plt.show()
-
 
 # Connect to Keysight signal generator (idVendor, idProduct) in decimal
 inst = usbtmc.Instrument(2391,11271)
@@ -47,28 +37,46 @@ inst = usbtmc.Instrument(2391,11271)
 print('Connected to device: ')
 print(inst.ask('*IDN?'))
 
+# Generates SCPI command to send a single pump pulse to the signal genearator
+pumpStrList = ['{:.3f}'.format(x) for x in singlePumpCycle]
+pumpStr = ','.join(pumpStrList)
+commandPump = 'data:arb pump,' + pumpStr
 
-dataStrList = ['{:.3f}'.format(x) for x in singlePumpCycle]
-dataStr = ','.join(dataStrList)
-commandPump = 'DATA:ARB pump,' + dataStr
+# Idem for probe segment
+probePoints = 10
+probeList = [probeAmp for x in range(probePoints)]
+probeStrList = ['{:.3f}'.format(x) for x in probeList]
+probeStr = ','.join(probeStrList)
+commandProbe = 'data:arb probe,' + probeStr
 
-print(commandPump)
 
-commandProbe = 'data:arb probe, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1'
+# Generates SCPI command which builds the pump-probe waveform on the signal generator
+pumpPoints = m.floor(pumpTime/periodL)*periodPoints
+pumpPulses = m.floor(pumpPoints/periodPoints)
+probeLength = m.floor((totalPoints - pumpPoints)/probePoints)
+
+pumpProbe = '"Cs_cycle","pump",%i,repeat,maintain,5,"probe",%i,repeat,maintain,5' % (pumpPulses,probeLength)
+charLen = str(len(pumpProbe))
+bytesLen = str(len(charLen))
+commandPumpProbe = 'data:seq #%s%s%s' % (bytesLen,charLen,pumpProbe)
+
+print(commandProbe)
+print(commandPumpProbe)
+
 
 inst.write('FUNC:ARB:SRATE 1E6')
 inst.write('FUNC:ARB:FILTER OFF')
 inst.write('FUNC:ARB:PTPEAK 6')
 
+
 inst.write(commandPump)
 inst.write(commandProbe)
+inst.write(commandPumpProbe)
 
-inst.write('data:sequence #267"Cs_cycle","pump",5,repeat,maintain,10,"probe",0,once,highAtStart,5')
 
 inst.write('FUNC:ARB Cs_cycle')
 inst.write('MMEM:STORE:DATA "INT:\Cs_cycle.seq"')
 inst.write('DATA:VOL:CLEAR')
-
 
 inst.write('MMEM:LOAD:DATA "INT:\Cs_cycle.seq"')
 inst.write('FUNC ARB')
