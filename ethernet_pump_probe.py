73290f98902d3e4e271ccbb9f56471d35734a074
Peter Koss <peter.koss@kuleuven.be>
2018-08-10 14:39:56 +0200

    basic ethernet communication script

--- ethernet_pump_probe.py
+++ ethernet_pump_probe.py
@@ -1,6 +1,10 @@
 import socket
 
-ip = "10.33.62.201"
+ip = "10.33.62.200"
 port = 5025
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((ip, devPort))
+sock.send(b"*IDN?\n")
+print(sock.recv(1024))
+
