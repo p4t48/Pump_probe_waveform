57ce8b9913525ba97583da4a629bc86eb8e36163
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2018-08-10 21:44:45 +0900

    simplified sock example

--- ethernet_pump_probe.py
+++ ethernet_pump_probe.py
@@ -3,4 +3,11 @@ import socket
 ip = "10.33.62.201"
 port = 5025
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((ip , port))
+
+qStr = sock.sendall(b'*IDN?')
+reply = sock.recv(4096)
+
+print (str(reply))
+sock.close()
